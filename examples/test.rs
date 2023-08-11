6447f88d5d586bc0dad966a2ed4ac9571ed6a204
LemonjamesD <lemon@lemonjamesd.com>
2023-08-11 18:17:37 -0400

    Added bevy_log

--- examples/test.rs
+++ examples/test.rs
@@ -1,6 +1,13 @@
+use bevy_log::LogPlugin;
 use strike::prelude::*;
 use strike::secrets::TOKEN;
 
 fn main() {
-    App::new().add_plugins(DiscordAppPlugin::new(TOKEN)).run();
+    App::new()
+        .add_plugins(LogPlugin {
+            level: Level::DEBUG,
+            filter: String::new(),
+        })
+        .add_plugins(DiscordAppPlugin::new(TOKEN))
+        .run();
 }
