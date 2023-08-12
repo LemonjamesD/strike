9216e639004bff7969415835d5901cc9417b8989
LemonjamesD <lemon@lemonjamesd.com>
2023-08-11 21:41:25 -0400

    Using Tasks

--- src/events.rs
+++ src/events.rs
@@ -1,15 +1,17 @@
+use crate::prelude::*;
 use crate::types::OpCode;
 use crate::types::API_URL;
-use crate::{prelude::*, TokioRuntime};
 use anyhow::Result;
 use bevy_ecs::prelude::*;
 use bevy_ecs::reflect::ReflectResourceFns;
+use bevy_tasks::IoTaskPool;
+use bevy_tasks::TaskPool;
 use deref_derive::Deref;
-use futures::StreamExt;
 use serde_json::Value;
 use std::net::TcpStream;
 use std::rc::Rc;
 use std::sync::Arc;
+use std::thread::spawn;
 use tungstenite::connect;
 use tungstenite::stream::MaybeTlsStream;
 use tungstenite::Message;
@@ -21,18 +23,10 @@ pub(crate) struct GetGateway(String);
 #[derive(Resource, Deref)]
 pub(crate) struct WebsocketStream(WebSocket<MaybeTlsStream<TcpStream>>);
 
-pub(crate) fn get_gateway(mut get_gateway: ResMut<GetGateway>, tokio_runtime: Res<TokioRuntime>) {
-    let handle = tokio_runtime.handle();
-    let _ = handle.enter();
-    tokio_runtime.block_on(get_gateway_url(&mut get_gateway));
-}
-
-async fn get_gateway_url(get_gateway: &mut GetGateway) {
-    let mut gotten_gateway = reqwest::get(format!("{}/gateway", API_URL))
-        .await
+pub(crate) fn get_gateway(mut get_gateway: ResMut<GetGateway>) {
+    let mut gotten_gateway = reqwest::blocking::get(format!("{}/gateway", API_URL))
         .expect("Failed to get")
         .json::<Value>()
-        .await
         .expect("Failed to parse json into value")["url"]
         .clone()
         .to_string();
@@ -42,12 +36,8 @@ async fn get_gateway_url(get_gateway: &mut GetGateway) {
     get_gateway.0 = gotten_gateway;
 }
 
-pub(crate) fn read_gateway(
-    mut commands: Commands,
-    get_gateway: Res<GetGateway>,
-    tokio_runtime: Res<TokioRuntime>,
-) {
-    info!("{}", get_gateway.0);
+pub(crate) fn connect_to_gateway(mut commands: Commands, get_gateway: Res<GetGateway>) {
+    info!("Gateway URL: {}", get_gateway.0);
     // Insert it into the resource
     let socket = connect(&get_gateway.0).unwrap();
     commands.insert_resource(WebsocketStream(socket.0));
@@ -63,25 +53,30 @@ pub(crate) fn read_stream(
     mut op_events: EventWriter<OpEvents>,
     mut stream: ResMut<WebsocketStream>,
 ) {
-    let read = stream.0.read();
-    if read.is_err() {
-        return;
-    }
-    match read.unwrap() {
-        Message::Text(text) => {
-            let valued: Value =
-                serde_json::from_str(&text).expect("Failed to unwrap Text event into JSON");
-            let op_code: Result<OpCode, _> = serde_json::from_str(&text);
-            if let Ok(op_code) = op_code {
-                let value = match op_code.op {
-                    10 => OpEvents::Op10(op_code),
-                    _ => return,
-                };
-                op_events.send(value);
+    let pool = IoTaskPool::init(|| TaskPool::new());
+    let results = pool.scope(move |s| {
+        s.spawn(async move {
+            let read = stream.0.read();
+            if read.is_err() {
+                return;
             }
-        }
-        _ => {}
-    }
+            match read.unwrap() {
+                Message::Text(text) => {
+                    let valued: Value =
+                        serde_json::from_str(&text).expect("Failed to unwrap Text event into JSON");
+                    let op_code: Result<OpCode, _> = serde_json::from_str(&text);
+                    if let Ok(op_code) = op_code {
+                        let value = match op_code.op {
+                            10 => OpEvents::Op10(op_code),
+                            _ => return,
+                        };
+                        op_events.send(value);
+                    }
+                }
+                _ => {}
+            }
+        });
+    });
 }
 
 pub(crate) struct EventsPlugin;
@@ -89,7 +84,9 @@ impl Plugin for EventsPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<GetGateway>()
             .add_event::<OpEvents>()
-            .add_systems(Startup, (get_gateway, read_gateway).chain())
-            .add_systems(Update, read_stream);
+            .add_systems(
+                Startup,
+                (get_gateway, connect_to_gateway, apply_deferred, read_stream).chain(),
+            );
     }
 }
